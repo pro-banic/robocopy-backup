4cde358095b8388ad11f2631d175fc99d0480bb9
Sebastian Berge <57839498+pro-banic@users.noreply.github.com>
2023-04-23 07:40:16 +0200

    init

---
+++ upload_robocopy-foldersync.ps1
@@ -0,0 +1,6 @@
+$sourcePath = "D:\Uploadscript\uhu"
+$destinationPath = "Z:"
+$logFile = "D:\Uploadscript\robocopy.log"
+
+# robocopy-Befehl zum Kopieren der neuen und geänderten Dateien
+robocopy $sourcePath $destinationPath /e /xo /np /log:$logFile
