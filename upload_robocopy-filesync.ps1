2e758c24c17f8e65815819c21bc23ab84bd9af78
Sebastian Berge <57839498+pro-banic@users.noreply.github.com>
2023-07-27 14:00:04 +0200

    Update upload_robocopy-filesync.ps1
    hetzner setup

--- upload_robocopy-filesync.ps1
+++ upload_robocopy-filesync.ps1
@@ -1,8 +1,8 @@
 # https://github.com/pro-banic/robocopy-backup
 
-$sourceFile = "D:\Uploadscript\uhu\test.txt"
-$destinationPath = "Z:"
-$logFile = "D:\Uploadscript\robocopy.log"
+$sourceFile = "C:\ProgramData\fiskaltrust\service\xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxx.sqlite"
+$destinationPath = "\\uxxxxxx.your-storagebox.de\backup\"
+$logFile = "C:\protelDaten\fiskaltrust-backup\robocopy.log"
 
 # robocopy-Befehl zum Kopieren der Datei
 robocopy $(Split-Path $sourceFile -Parent) $destinationPath $(Split-Path $sourceFile -Leaf) /np /log:$logFile
